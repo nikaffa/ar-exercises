c6ef2654acf1c74d698806907c7f4c1d354e6f9d
Veronika Fatikhova <fidelio@list.ru>
2022-01-04 22:31:02 -0800

    setup and created ar_exercises database

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,4 @@
+#Creates Active Record Employee model, mapped to an employees table at the database
 class Employee < ActiveRecord::Base
 
 end
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,4 @@
+#Creates Active Record Store model, mapped to a stores table at the database
 class Store < ActiveRecord::Base
 
 end
