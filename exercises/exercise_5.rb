222174c3134ce3eb959f41f838901c5b77c2a65a
Veronika Fatikhova <fidelio@list.ru>
2022-01-05 09:01:24 -0800

    done exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,11 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+# find the total revenue for the entire company (all stores)
+puts Store.sum("annual_revenue")
+
+#the average annual revenue for all stores
+puts Store.average("annual_revenue")
+
+#the number of stores that are generating $1M or more in annual sales
+puts Store.where('annual_revenue >= 1000000').count
