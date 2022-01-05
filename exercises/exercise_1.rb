f1c6952e56925daca4d5128a9f8cef555a01ef55
Veronika Fatikhova <fidelio@list.ru>
2022-01-04 23:18:19 -0800

    update exercise 1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -9,8 +9,9 @@ puts "----------"
 #employees
 #"id" serial primary key, "store_id" integer, "first_name" character varying, "last_name" character varying, "hourly_rate" integer, "created_at" timestamp NOT NULL, "updated_at" timestamp NOT NULL 
 
-Burnaby = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
-Richmond = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
-Gastown = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
 
+puts "----------"
 puts Store.count
